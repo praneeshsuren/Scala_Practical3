8804a4ef89da9be4e36bcc0edfcbacd4e27e1d91
praneeshsuren <praneeshsuren@gmail.com>
2024-07-01 18:35:10 +0530

    feat: added code for Q1

---
+++ Q1.scala
@@ -0,0 +1,10 @@
+def reverseString(str : String) : String = {
+    if (str.isEmpty) ""
+    else reverseString(str.tail) + str.head
+}
+
+@main def main1() : Unit = {
+    println("Enter a string to reverse: ");
+    val str = scala.io.StdIn.readLine();
+    println("Reversed string: " + reverseString(str));
+}
